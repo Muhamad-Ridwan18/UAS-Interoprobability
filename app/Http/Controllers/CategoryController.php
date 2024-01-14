b20b0ef15120698b8b9db8f873febcc8ec8be107
Rangga Eka Kusuma Dani <ranggaeka165@gmail.com>
2024-01-14 13:56:16 +0700

    Add function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -17,5 +17,33 @@ class CategoryController extends Controller
         //
     }
 
-    //
+    public function index(){
+      $categories = Category::all();
+      return response()->json($categories);
+    }
+
+    public function store(Request $request){
+      $this->validate($request, [
+        'name' => 'required|unique:categories'
+      ]);
+
+      $category = Category::create($request->all());
+      return response()->json($category);
+    }
+
+    public function update(Request $request, $id){
+      $this->validate($request, [
+        'name' => 'required|unique:categories'
+      ]);
+
+      $category = Category::find($id);
+      $category->update($request->all());
+      return response()->json($category);
+    }
+
+    public function destroy($id){
+      $category = Category::find($id);
+      $category->delete();
+      return response()->json($category);
+    }
 }
